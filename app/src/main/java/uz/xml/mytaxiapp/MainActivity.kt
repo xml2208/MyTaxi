36d7fbdb9f9185a371bc0b14ecd3280a5ed33eb9
xml2208 <mohinabonu2208@gmail/com>
2024-08-07 11:36:29 +0500

    initial commit

---
+++ app/src/main/java/uz/xml/mytaxiapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package uz.xml.mytaxiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import uz.xml.mytaxiapp.ui.theme.MyTaxiAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyTaxiAppTheme {
+
+            }
+        }
+    }
+}
